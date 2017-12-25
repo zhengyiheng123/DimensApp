512018579e437cab51d81857d28a8b1e0f1144ec
郑宜衡 <15621599930@163.com>
2017-12-25 22:54:07 +0800

    来自zhengyiheng1234的更新

--- app/src/main/java/com/example/asus/dimensapp/test/FirstTestClass.java
+++ app/src/main/java/com/example/asus/dimensapp/test/FirstTestClass.java
@@ -4,10 +4,4 @@ package com.example.asus.dimensapp.test;
  * Created by ASUS on 2017/12/25.
  */
 public class FirstTestClass {
-private String name;
-    private String age;
-    public static void main(String[] arg){
-        System.out.print("234");
-        System.out.print("5");
-    }
 }
--- app/src/main/java/com/example/asus/dimensapp/test/SecondTestClass.java
+++ app/src/main/java/com/example/asus/dimensapp/test/SecondTestClass.java
@@ -4,8 +4,4 @@ package com.example.asus.dimensapp.test;
  * Created by ASUS on 2017/12/25.
  */
 public class SecondTestClass {
-    public static void main(String[] arg){
-        System.out.print("234");
-        System.out.print("5");
-    }
 }
