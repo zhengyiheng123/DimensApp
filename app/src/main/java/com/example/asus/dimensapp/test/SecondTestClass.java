e1b86279afb9f5f896da7271b3315a81fcf82e77
郑宜衡 <15621599930@163.com>
2017-12-25 22:29:48 +0800

    来自zhengyiheng1234的更新

---
+++ app/src/main/java/com/example/asus/dimensapp/test/SecondTestClass.java
@@ -0,0 +1,10 @@
+package com.example.asus.dimensapp.test;
+
+/**
+ * Created by ASUS on 2017/12/25.
+ */
+public class SecondTestClass {
+    public static void main(String[] arg){
+        System.out.print("123");
+    }
+}
